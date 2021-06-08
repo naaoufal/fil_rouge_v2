c280b489290875b25b1de08ec9fcf65caa50eb4b
naaoufal <naoufelbenmensour@gmail.com>
2021-06-08 23:23:40 +0100

    add files

---
+++ front_end/src/ClientComponent/SideBar.js
@@ -0,0 +1,56 @@
+import { Link, useHistory } from "react-router-dom";
+
+function SideBar () {
+
+    // init history:
+    const history = useHistory()
+
+    // check token if exist:
+    const token = localStorage.getItem('token')
+    const info = JSON.parse(localStorage.getItem('adminInfo'))
+
+    // if(token) {
+    //     // put some code here
+    // } else {
+    //     history.push("/AdminLogin")
+    // }
+
+    return (
+        <aside>
+            <div id="sidebar" class="">
+                <ul class="sidebar-menu" id="nav-accordion">
+                <p class="centered"><Link><img src="" class="img-circle" width="80" /></Link></p>
+                {/* <h5 class="centered">{info.firstname} {info.lastname}</h5> */}
+                <br /> <br />
+                    <li>
+                        <Link to="/AdminDashboard">
+                        <span>Informations Générales</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/staff">
+                        <span>Gestion des Modérateurs</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/posts">
+                        <span>Gestion des Posts</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/tags">
+                        <span>Gestion des Tags et Themes</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contacts">
+                        <span>Gestion des Contacts</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+    )
+}
+
+export default SideBar
--- front_end/src/ClientComponent/home.js
+++ front_end/src/ClientComponent/home.js
@@ -4,19 +4,23 @@ function UserHome () {
     return (
         <section id="container">
             <Header />
-            <section id="main-content">
+            <section id="main">
                 <section class="wrapper site-min-height">
                     <div class="chat-room mt">
                         <aside class="mid-side">
                             <div class="chat-room-head">
-                                <h3>Question</h3>
+                                <h3>Tous Les Question</h3>
                                 <form action="#" class="pull-right position">
-                                    <input type="text" placeholder="Search" class="form-control search-btn " />
+                                    <input type="text" placeholder="Search" class="form-control search-btn" />
                                 </form>
                             </div>
                             <div className="room-desk">
-                                <h4 class="pull-left">Les Questions Les Plus Fréquentes</h4>
-                                <a href="#" class="pull-right btn btn-theme02">Poser Une Question</a>
+                                <a href="#" class="pull-right btn btn-theme">Poser Une Question</a>
+                                <div class="room-box">
+                                    <h5 class="text-primary"><a href="">Dashboard</a></h5>
+                                    <p>We talk here about our dashboard. No support given.</p>
+                                    <p><span class="text-muted">Admin :</span> Sam Soffes | <span class="text-muted">Members :</span> 98 | <span class="text-muted">Last Activity :</span> 2 min ago</p>
+                                </div>
                                 <div class="room-box">
                                     <h5 class="text-primary"><a href="">Dashboard</a></h5>
                                     <p>We talk here about our dashboard. No support given.</p>
