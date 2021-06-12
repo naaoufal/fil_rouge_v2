c286c0a43aeb4658565b6d16c2c3f3efe6e3f142
naaoufal <naoufelbenmensour@gmail.com>
2021-06-12 22:56:51 +0100

    add files

---
+++ back_end/models/posts.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const postsSchema = new mongoose.Schema({
+
+    title : {
+        type : String,
+        required : true
+    },
+    desc : {
+        type : String,
+        required : true
+    },
+    user_id : {
+        type : String,
+        required : true
+    },
+    is_valid : {
+        type : String,
+        required : true
+    },
+    createdAt : {
+        type : Date,
+        required : true
+    }
+
+})
+
+module.exports = mongoose.model('posts', postsSchema)
