60a12f57ff1e89556d2d7f35a79270ca3c60d945
naaoufal <naoufelbenmensour@gmail.com>
2021-06-14 17:23:17 +0100

    add files

--- front_end/src/ClientComponent/home.js
+++ front_end/src/ClientComponent/home.js
@@ -15,7 +15,31 @@ function UserHome () {
                                 </form>
                             </div>
                             <div className="room-desk">
-                                <a href="#" class="pull-right btn btn-theme">Poser Une Question</a>
+                                <button data-toggle="modal" data-target="#exampleModal" class="pull-right btn btn-theme">Poser Une Question</button>
+                                {/* modal start */}
+                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div className="group-control">
+                                                <label>Ecrire ton Problème ...</label>
+                                                <textarea class="form-control placeholder-no-fix"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-theme">Save changes</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* modal end */}
                                 <div class="room-box">
                                     <h5 class="text-primary"><a href="">Dashboard</a></h5>
                                     <p>We talk here about our dashboard. No support given.</p>
