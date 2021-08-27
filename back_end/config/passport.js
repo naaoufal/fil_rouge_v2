97b595bddc0e5cbd852091e029b5d32bc68d0145
benmansour <naoufelbenmensour@gmail.com>
2021-08-27 09:32:35 +0100

    add config

---
+++ back_end/config/passport.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+require("dotenv").config();
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3001/auth/google",
+  //passReqToCallback: true,
+},
+function(accessToken, refreshToken, profile, done) {
+    console.log(profile)
+    //return done(null, profile);
+    // here we save data that come from google !!!
+    return done(null, profile)
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const connectDB = require("./back_end/config/mongodb");
 const app = express();
 const http = require('http');
+const passport = require('passport');
+require("./back_end/config/passport")
 const server = http.createServer(app)
 // init socket :
 const socket = require("socket.io");
@@ -12,18 +14,30 @@ const io = socket(server)
 // connect to database with mongodb:
 connectDB()
 
+// config cors :
 app.use(express.json())
 app.use(cors())
 
-// socket io config :
-io.on("connection", socket => {
-    socket.emit('ID', socket.id)
-    //socket.emit('firstname', socket.name)
-    socket.on("send message", body => {
-        io.emit("message", body)
-    })
-})
+// check if user auth exist or not :
+const isLoggedIn = (req, res, next) => {
+    req.user ? next() : res.sendStatus(401);
+}
 
+// passport config :
+// app.use(session({ secret: 'google', resave: false, saveUninitialized: true }))
+app.use(passport.initialize())
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: 
+    [ 'openid', 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+  passport.authenticate( 'google', {
+    successRedirect: '/',
+    // failureRedirect: '/auth/google/failure'
+  })
+);
 
 // declaring url endpoints :
 app.use("/api/admins", require("./back_end/routes/admins"))
@@ -31,6 +45,8 @@ app.use("/api/tags", require("./back_end/routes/tags"))
 app.use("/api/staffs", require("./back_end/routes/staffs"))
 app.use("/api/clients", require("./back_end/routes/clients"))
 app.use("/api/posts", require("./back_end/routes/posts"))
+
+// config for comments :
 
 
 // start the server
